390db67acc13627b4bb056a6053c780abf286b31
SantinoVicentini <vicen.lan2017@gmail.com>
2021-03-29 10:24:12 -0300

    Changes

--- peculiar-test.py
+++ peculiar-test.py
@@ -1,10 +1,10 @@
-def misma_paridad(n:int, m:int) -> str:
+def misma_paridad(n:int, m:int) -> bool:
 
     if n % 2 == 0 and m % 2 == 0 or n % 2 == 1 and m % 2 == 1: 
-        vr = 'si'
+        vr = True
 
     else: 
-        vr = 'no'   
+        vr = False  
     
     return vr
 
@@ -15,13 +15,13 @@ class Misma(unittest.TestCase):
 #   A PARTIR DE ACA USTEDES ESCRIBAN LOS TEST
     
     def test_verdaderos(self):
-        self.assertEqual(misma_paridad(2,2),'si')
-        self.assertEqual(misma_paridad(3,9),'si')
+        self.assertTrue(misma_paridad(2,2))
+        self.assertTrue(misma_paridad(3,9))
         #NUEVO TEST
         
         
     def test_falsos(self):
-        self.assertEqual(misma_paridad(2,3),'no')
+        self.assertFalse(misma_paridad(2,3))
         #NUEVO TEST
 
 # esta linea es para correr los test previamente declarados
--- peculiar.py
+++ peculiar.py
@@ -1,4 +1,4 @@
-def misma_paridad(n:int, m:int) -> str:
+def misma_paridad(n:int, m:int) -> bool:
     '''
     Especificaciones: determinar si los parámetros son pares o impares
     Encabezado: def misma_paridad(n:int, m:int) -> str
@@ -6,28 +6,26 @@ def misma_paridad(n:int, m:int) -> str:
     Postcondiciones: vr será un string “si” solo si ambos parámetros (n,m) son pares o impares, sin embargo, será “no” si difieren entre sí.
     '''
     if n % 2 == 0 and m % 2 == 0 or n % 2 == 1 and m % 2 == 1: 
-        vr = 'si'
+        vr = True
     else: 
-        vr = 'no'
+        vr = False
     return vr
 
 #print(misma_paridad(4,4))
 
-def alterna_paridad(x:int)->str:
+def alterna_paridad(x:int)->bool:
     num:str = str(x)
-    vr:str = ''
+    vr:bool = True
     numAnterior:int = int(num[0]) % 2
-    if x < 10:
-        vr = 'si'
-
+    
     i:int = 1
     while i < len(num):
         if numAnterior != int(num[i]) % 2:
             numAnterior = int(num[i]) % 2
             i = i + 1
-            vr = 'si'
+            vr
         else:
-            vr = 'no'
+            vr = False
             break
     return vr
-print(alterna_paridad(101011))
+print(alterna_paridad(123455))
