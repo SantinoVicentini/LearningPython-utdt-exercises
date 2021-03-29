1ff13ace2dc0db3888e400488685cd49a9bbe3bf
SantinoVicentini <vicen.lan2017@gmail.com>
2021-03-29 12:03:59 -0300

    Changes

--- peculiar.py
+++ peculiar.py
@@ -5,13 +5,11 @@ def misma_paridad(n:int, m:int) -> bool:
     Precondiciones: n y m deben pertenecer a los N + {0}
     Postcondiciones: vr será un string “si” solo si ambos parámetros (n,m) son pares o impares, sin embargo, será “no” si difieren entre sí.
     '''
-    if n % 2 == 0 and m % 2 == 0 or n % 2 == 1 and m % 2 == 1: 
-        vr = True
-    else: 
-        vr = False
+    vr:bool = True
+    vr = vr and (n % 2 == 0 and m % 2 == 0 or n % 2 == 1 and m % 2 == 1)
     return vr
 
-#print(misma_paridad(4,4))
+print(misma_paridad(1,0))
 
 def alterna_paridad(x:int)->bool:
     num:str = str(x)
@@ -24,4 +22,4 @@ def alterna_paridad(x:int)->bool:
         i = i + 1
          
     return vr
-print(alterna_paridad(10122))
+print(alterna_paridad(10121))
