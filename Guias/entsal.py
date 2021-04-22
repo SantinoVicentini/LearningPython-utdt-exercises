d9ee0c39d91a7f4a53a9eb2d71d123118b23cd5b
SantinoVicentini <vicen.lan2017@gmail.com>
2021-04-22 00:15:07 -0300

    repaso

--- Guias/entsal.py
+++ Guias/entsal.py
@@ -19,17 +19,19 @@ from typing import TextIO
 #f.close()
 
 num:str = input('que numero deseas evaluar?')
-numero:int = int(num)
 
-vr:str = 'correcto'
-i:int = 1
-while i <= 10:
-    resultado:str = input('cuanto es ' + str(numero) + ' por ' + str(i) + ' ?')
-    resulv2:int = int(resultado)
-    if resulv2 == i * numero:
-        print(vr)
-    if i * numero != resulv2:
-        vr = 'incorrecto ' + 'es ' + str(i*numero)
-        print(vr)
-    i += 1
+def tablas(num:str):
+    numero:int = int(num)
+    i:int = 1
+    while i <= 10:
+        resultado:str = input('cuanto es ' + str(numero) + ' por ' + str(i) + ' ?')
+        resulv2:int = int(resultado)
+        if resulv2 == i * numero:
+            print('correcto')
+        if resulv2 != i * numero:
+            print('incorrecto')
+            
+        i += 1
+
+tablas(num)
 #Terminar
---
+++ repaso/functions.py
@@ -0,0 +1,19 @@
+from typing import TextIO, List
+
+def abrirArchivo(nombreDeArchivo:str)->list[str]:
+    '''
+    Abre y lee un archivo
+    pre: El archivo debe ser existente, contener formaato de texto y encontrarse en el mismo directorio
+    post: va a devolver una lista con todos los datos del archivo
+    '''
+    vr:list[str] = []
+    f:TextIO = open(nombreDeArchivo, encoding='utf-8')
+    linea:str
+    for linea in f:
+        linea = linea.rstrip()
+        vr.append(linea)
+
+    return print(vr)    
+
+abrirArchivo('provincias.csv')
+ 
