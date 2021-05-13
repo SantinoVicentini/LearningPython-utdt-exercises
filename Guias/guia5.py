6152fe398c2e98bea11eb7e04edaec63611aeb9f
SantinoVicentini <vicen.lan2017@gmail.com>
2021-05-12 23:18:31 -0300

    guia5

--- Guias/guia5.py
+++ Guias/guia5.py
@@ -35,13 +35,24 @@ def permitidos(s:str)->bool:
         i+=1
     return vr
 
-def permitidosv(s:int)->bool:
+def numeros()->List[str]:
+    vr:List[str] = []
+    i:int = 1
+    while i <= 999999:
+        vr.append(str(i))
+        i += 1
+    return vr
+
+def permitidosv(a:List[str])->bool:
     i:int = 0
-    numeros:List[str] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    numeross:List[str] = numeros()
     vr:bool = True
-    while i < s:
-        vr = vr and s in numeros
-        i+=1
+    while i < len(a):
+        if a[i] in numeross:
+            vr
+        else:
+            vr = False
+        i +=1
     return vr
 
 def consola():
@@ -54,13 +65,12 @@ def consola():
             lista.append(sys.argv[i])
             i+=1
         sys.argv.insert(2, lista)
-        j:int = 0
-        while j < len(lista):
-            if not permitidos(lista[j]) and listaOrdenada(lista):
-                print('si')
-            else:
-                print('no')
-            j+=1
+        if permitidosv(lista) and listaOrdenada(lista):
+            print('ordenanda')
+        elif not permitidosv(lista):
+            print('error en el caracter ingresado')
+
+
 
     elif nombre_funcion == 'g':
         if len(sys.argv) > 3 or not permitidos(sys.argv[2]):
